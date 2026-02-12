bd76b8b1088a1a143c9efd637699729f222866d3
Mikawo846 <yashin_mihail@bk.ru>
2026-02-12 14:45:54 +0300

    Revert "Добавлен BOM к HTML файлам для исправления кодировки"
    This reverts commit 4ac17d972bc6a125adb3c31feae65bac819ed025.

--- add_bom.ps1
+++
@@ -1,7 +0,0 @@
-# Script to add UTF-8 BOM to all HTML files
-
-Get-ChildItem -Path "." -Filter "*.html" -Recurse | ForEach-Object {
-    $file = $_.FullName
-    $content = Get-Content -Path $file -Raw -Encoding UTF8
-    [System.IO.File]::WriteAllText($file, $content, [System.Text.Encoding]::UTF8)
-}
